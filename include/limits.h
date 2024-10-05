234b75fdd999207912b7f834b511d4792c70e410
Aaron Gill-Braun <aarongillbraun@gmail.com>
2024-10-05 15:16:26 -0400

    adjusted some system limits

--- include/limits.h
+++ include/limits.h
@@ -44,11 +44,11 @@
 #ifndef NAME_MAX
 #define NAME_MAX 255
 #endif
-#define PATH_MAX 4096
+#define PATH_MAX 1024
 #define NGROUPS_MAX 32
-#define ARG_MAX 131072
+#define ARG_MAX 32
 #define IOV_MAX 1024
-#define SYMLOOP_MAX 40
+#define SYMLOOP_MAX 10
 #define WORD_BIT 32
 #define SSIZE_MAX LONG_MAX
 #define TZNAME_MAX 6
@@ -93,7 +93,7 @@
 
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE) || defined(_XOPEN_SOURCE)
 
-#ifdef PAGESIZE
+#if defined(PAGESIZE) && !defined(PAGE_SIZE)
 #define PAGE_SIZE PAGESIZE
 #endif
 #define NZERO 20
